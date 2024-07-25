1a617355aaed8ce5803b9f58b7c62c10608410f7
chaimaraach <chaima.raach@speedykom.de>
2024-07-25 20:55:28 +0100

    added handlers

--- main.go
+++ main.go
@@ -6,6 +6,29 @@ import (
 	"net/http"
 )
 
+func helloHandler(w http.ResponseWriter, r *http.Request){
+	if r.URL.Path != "/hello" {
+		htttp.Error(w, "404 not found", http.StatusNotFound)
+		return
+	}
+	if r.method != "GET" {
+		htttp.Error(w, "Method not supported", http.StatusNotFound)
+		return
+	}
+
+	fmt.Fprintf("Hello")
+}
+func formHandler(w http.ResponseWriter, r "http.Request"){
+	if err := r.Parseform(); err != nil {
+	fmt.Fprintf(w, "ParseForm() err: %v", err)
+	return
+	
+	fmt.Fprintf(w, "POST request successful")
+	hame := r.FormValue("name")
+	address := r.FormValue("address")
+	fmt.Fprintf(w, "Name = %s\n", name)
+	fmt.Fprintf(w, "Address -", address)}
+
 func main() {
 	fileServer := http.fileServer(http.Dir("./static"))
 	http.Handle("/",fileServer)
