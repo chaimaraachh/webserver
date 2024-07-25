591d268a460c6b6cb7835ab264c3405312f6e250
chaimaraach <chaima.raach@speedykom.de>
2024-07-25 21:05:11 +0100

    fixed typos

--- main.go
+++ main.go
@@ -6,41 +6,40 @@ import (
 	"net/http"
 )
 
-func helloHandler(w http.ResponseWriter, r *http.Request){
+func helloHandler(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/hello" {
-		htttp.Error(w, "404 not found", http.StatusNotFound)
+		http.Error(w, "404 not found", http.StatusNotFound)
 		return
 	}
-	if r.method != "GET" {
-		htttp.Error(w, "Method not supported", http.StatusNotFound)
+	if r.Method != "GET" {
+		http.Error(w, "Method not supported", http.StatusMethodNotAllowed)
 		return
 	}
 
-	fmt.Fprintf("Hello")
+	fmt.Fprintf(w, "Hello")
 }
-func formHandler(w http.ResponseWriter, r "http.Request"){
-	if err := r.Parseform(); err != nil {
-	fmt.Fprintf(w, "ParseForm() err: %v", err)
-	return
-	
-	fmt.Fprintf(w, "POST request successful")
-	hame := r.FormValue("name")
+
+func formHandler(w http.ResponseWriter, r *http.Request) {
+	if err := r.ParseForm(); err != nil {
+		fmt.Fprintf(w, "ParseForm() err: %v", err)
+		return
+	}
+
+	fmt.Fprintf(w, "POST request successful\n")
+	name := r.FormValue("name")
 	address := r.FormValue("address")
 	fmt.Fprintf(w, "Name = %s\n", name)
-	fmt.Fprintf(w, "Address -", address)}
+	fmt.Fprintf(w, "Address = %s\n", address)
+}
 
 func main() {
-	fileServer := http.fileServer(http.Dir("./static"))
-	http.Handle("/",fileServer)
-	http.HandleFunc("/form",formHandler)
-	http.HandleFunc("/hello",helloHandler)
+	fileServer := http.FileServer(http.Dir("./static"))
+	http.Handle("/", fileServer)
+	http.HandleFunc("/form", formHandler)
+	http.HandleFunc("/hello", helloHandler)
 
-
-	fmt.Printf("Starting server at port 8080")
-
-	if err := http.ListenAndServer(":8080", nil); err != nil {
+	fmt.Printf("Starting server at port 8080\n")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal(err)
 	}
-
-
 }
